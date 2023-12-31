a526c332132ad99d37f86feb7e3536c263d64e39
Aniol Molero <aniolmolerograu@gmail.com>
2023-12-31 17:11:25 +0100

    Deixat el codi molt més maco, i amb entrada de paràmetres

--- src/main/scala/CrowdedChessboard.scala
+++ src/main/scala/CrowdedChessboard.scala
@@ -1,8 +1,8 @@
+import scala.Console.{GREEN, RESET}
+import scala.io.StdIn.readLine
+
 
 object CrowdedChessboard extends App{
-  //TODO: Posar tota la part de generació de contraints inicials en una funció per poder-ho reutilitzar
-  var e = new ScalAT("CrowdedChessboard")
-  var e2: ScalAT = null
 
   val instancies: Array[Array[Int]] = Array(
     Array(5,5,5,8,5),
@@ -23,6 +23,19 @@ object CrowdedChessboard extends App{
   var unsat = false;
   var placeKings = true;
   var instanciaEscollida = 5 //De 0 a 11
+
+
+  println("Escull l'instància a resoldre (0-11):")
+  instanciaEscollida = readLine().toInt
+  println("Escull si el problema ha de ser satisfactible o no, afegint un cavall (0 = Insatisfactible, 1 = Satisfactible):")
+  unsat = if(readLine().toInt == 0) true else false
+  println("Escull l'encoding a utilitzar (0 = Quadràtic, 1 = Logarítmic):")
+  configuracioLog = if(readLine().toInt == 0) false else true
+  if (!unsat) {
+    println("Escull si vols buscar el màxim nombre de reis que es poden col·locar al tauler (0: No, 1: Sí):")
+    placeKings = if(readLine().toInt == 0) false else true
+  }
+
   //Mida tauler
   val n = instancies(instanciaEscollida)(0)
 
@@ -33,27 +46,24 @@ object CrowdedChessboard extends App{
   if(unsat) nCavalls += 1
 
   /* objecte(i)(j) cert si la casella i j conté una peça de tipus objecte*/
-  /*var reines: Array[Array[Int]] = e.newVar2DArray(n, n)
-  var torres: Array[Array[Int]] = e.newVar2DArray(n, n)
-  var alfils: Array[Array[Int]] = e.newVar2DArray(n, n)
-  var cavalls: Array[Array[Int]] = e.newVar2DArray(n, n)
-  var diagonals: Array[Int] = e.newVarArray(2*n-1) //Variables que indiquen si una diagonal té un alfil
-  var contradiagonals: Array[Int] = e.newVarArray(2*n-1); //Variables que indiquen si una contradiagonal té un alfil*/
+  // Queden lligats a un ScalAT
   var reines: Array[Array[Int]] = Array.ofDim(n,n)
   var torres: Array[Array[Int]] = Array.ofDim(n,n)
   var alfils: Array[Array[Int]] = Array.ofDim(n,n)
   var cavalls: Array[Array[Int]] = Array.ofDim(n,n)
+  var reis: Array[Array[Int]] = Array.ofDim(n, n)
+
   var diagonals: Array[Int] = Array.ofDim(2 * n - 1) //Variables que indiquen si una diagonal té un alfil
   var contradiagonals: Array[Int] = Array.ofDim(2 * n - 1); //Variables que indiquen si una contradiagonal té un alfil
 
-  var reinesAux: Array[Array[Boolean]] =  Array.ofDim(n,n)
-  var torresAux: Array[Array[Boolean]] =  Array.ofDim(n,n)
-  var alfilsAux: Array[Array[Boolean]] =  Array.ofDim(n,n)
-  var cavallsAux: Array[Array[Boolean]] =  Array.ofDim(n,n)
-
-  var reis: Array[Array[Int]] = Array.ofDim(n, n)
-  //var reis: Array[Array[Int]] = Array.ofDim(n, n)
+  // A la posició (x)(y) hi ha cert si a la casella x y hi ha la peça corresponent
+  // No estan lligats a cap ScalAT
+  var reinesAux: Array[Array[Boolean]] = Array.ofDim(n,n)
+  var torresAux: Array[Array[Boolean]] = Array.ofDim(n,n)
+  var alfilsAux: Array[Array[Boolean]] = Array.ofDim(n,n)
+  var cavallsAux: Array[Array[Boolean]] = Array.ofDim(n,n)
   var reisAux: Array[Array[Boolean]] =  Array.ofDim(n,n)
+
 
   def afegirConstraints(e: ScalAT): Unit = {
     reines = e.newVar2DArray(n, n)
@@ -177,14 +187,13 @@ object CrowdedChessboard extends App{
 
     // ALFILS
     // Només hi ha alfils als extrems del tauler
-    for (i <- 1 until n - 1) {
-      for (j <- 1 until n - 1) {
-        e.addClause(-alfils(i)(j) :: List())
+    if(nAlfils == 2*(n-1)) {
+      for (i <- 1 until n - 1) {
+        for (j <- 1 until n - 1) {
+          e.addClause(-alfils(i)(j) :: List())
+        }
       }
     }
-
-    //e.addEOLog(alfils(0)(0) :: alfils(n-1)(n-1) :: List())
-    //e.addEOLog(alfils(0)(n-1) :: alfils(n-1)(0) :: List())
 
     //A cada contradiagonal hi ha com a molt un alfil
     for (v <- 0 to 2 * n - 2) {
@@ -213,14 +222,6 @@ object CrowdedChessboard extends App{
       e.addEK(diagonals.toList, 2 * (n - 1))
 
       //Reifiquem (????????????????????????) caselles amb alfil i contradiagonals
-      //Amb això va més ràpid per alguna raó
-      /*for (v <- -n + 1 until n) {
-        var casellesContradiag = (for (i <- 0 until n; j <- 0 until n; if i - j == v) yield alfils(i)(j)).toList;
-        for (i <- casellesContradiag.indices) e.addClause(-casellesContradiag(i) :: diagonals(v + n - 1) :: List())
-        //e.addClause(List(-contradiagonals(v + n - 1)).concat(casellesContradiag))
-        e.addALO(List(-contradiagonals(v + n - 1)).concat(casellesContradiag))
-      }*/
-
       for (v <- 0 to 2 * n - 2) {
         var casellesContradiag = (for (i <- 0 until n; j <- 0 until n; if i + j == v) yield alfils(i)(j)).toList;
         for (i <- casellesContradiag.indices) e.addClause(-casellesContradiag(i) :: contradiagonals(v) :: List())
@@ -232,8 +233,6 @@ object CrowdedChessboard extends App{
       e.addEK(contradiagonals.toList, 2 * (n - 1))
     }
 
-
-
     // CAVALLS
 
     //val possibleMoves = List((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
@@ -244,194 +243,13 @@ object CrowdedChessboard extends App{
         else e.addAMOQuad(List(cavalls(x)(y), cavalls(i._1 + x)(i._2 + y)))
     //e.addClause(-cavalls(x)(y) :: -cavalls(i._1+x)(i._2+y) :: List())
   }
-  afegirConstraints(e)
-  /*
-  // GENÈRIQUES
-  for (i <- 0 until n)
-    for (j <- 0 until n) {
-      val l = reines(i)(j) :: torres(i)(j) :: alfils(i)(j) :: cavalls(i)(j) :: List()
-      if(configuracioLog) e.addAMOLog(l)
-      else e.addAMOQuad(l)
-    }
 
-  // CONSTRAINTS SEGONS ENUNCIAT
-  e.addEK(reines.flatten.toList, nReines)
-  e.addEK(torres.flatten.toList, nTorres)
-  e.addEK(alfils.flatten.toList, nAlfils)
-  //e.addEK((for(i <- 0 until n; j <- 0 until n; if(i == 0 | j == 0 | i == n-1 | j == n-1)) yield alfils(i)(j)).toList, nAlfils)
-  e.addEK(cavalls.flatten.toList, nCavalls)
-
-  // REINES
-  //A cada fila hi ha una reina
-  for (i <- reines) {
-    if(nReines >= n) {
-      if(configuracioLog) e.addEOLog(i.toList)
-      else e.addEOQuad(i.toList)
-    }
-    else {
-      if(configuracioLog) e.addAMOLog(i.toList)
-      else e.addAMOQuad(i.toList)
-    }
-  }
-
-  //A cada columna hi ha una reina
-  for (i <- reines.transpose)
-    if(nReines >= n) {
-      if(configuracioLog) e.addEOLog(i.toList)
-      else e.addEOQuad(i.toList)
-    }
-    else {
-      {
-        if(configuracioLog) e.addAMOLog(i.toList)
-        else e.addAMOQuad(i.toList)
-      }
-    }
-
-  //A cada contradiagonal hi ha com a molt una reina
-  for (v <- 0 to 2 * n - 2) {
-    val l = (for (i <- 0 until n; j <- 0 until n; if i + j == v) yield reines(i)(j)).toList
-    if(configuracioLog) e.addAMOLog(l)
-    else e.addAMOQuad(l)
-  }
-
-  //A cada diagonal hi ha com a molt una reina
-  for (v <- -n + 1 until n) {
-    val l = (for (i <- 0 until n; j <- 0 until n; if i - j == v) yield reines(i)(j)).toList
-    if(configuracioLog) e.addAMOLog(l)
-    else e.addAMOQuad(l)
-  }
-
-  //Trencament de simetries
-  //Original: Doble sat, meitat unsat
-  /*
-  for(i <- 0 until n )
-    for(j <- i+1 until n){
-      e.addClause(-reines(1)(i) :: -reines(n-1)(j) :: List())
-      e.addClause(-reines(i)(1) :: -reines(j)(n-1) :: List())
-    }*/
-  //Lleuger augment sat, gran decrement unsat
-  for (i <- 0 until n)
-    for (j <- i + 1 until n) {
-      //e.addClause(-reines(2)(i) :: -reines(1)(j) :: List())
-      //e.addClause(-reines(i)(2) :: -reines(j)(1) :: List())
-
-      if(configuracioLog) {
-        e.addAMOLog(reines(2)(i) :: reines(1)(j) :: List())
-        e.addAMOLog(reines(i)(2) :: reines(j)(1) :: List())
-      }
-      else {
-        e.addAMOQuad(reines(2)(i) :: reines(1)(j) :: List())
-        e.addAMOQuad(reines(i)(2) :: reines(j)(1) :: List())
-      }
-    }
-  //Més del doble sat, menys del doble unsat
-  /*
-  for (i <- 0 until n)
-    for (j <- i + 1 until n) {
-      e.addClause(-reines(n - 1)(i) :: -reines(1)(j) :: List())
-      e.addClause(-reines(i)(n - 1) :: -reines(j)(1) :: List())
-    }*/
-
-
-  // TORRES
-  //A cada fila hi ha una torre
-  for (i <- torres) {
-    if(nTorres >= n) {
-      if(configuracioLog) e.addEOLog(i.toList)
-      else e.addEOQuad(i.toList)
-    }
-    else {
-      if(configuracioLog) e.addAMOLog(i.toList)
-      else e.addAMOQuad(i.toList)
-    }
-  }
-
-  //A cada columna hi ha una torre
-  for (i <- torres.transpose) {
-    if(nTorres >= n) {
-      if(configuracioLog) e.addEOLog(i.toList)
-      else e.addEOQuad(i.toList)
-    }
-    else {
-      if(configuracioLog) e.addAMOLog(i.toList)
-      else e.addAMOQuad(i.toList)
-    }
-  }
-
-  // ALFILS
-  // Només hi ha alfils als extrems del tauler
-  for(i <- 1 until n-1){
-    for(j <- 1 until n-1){
-      e.addClause(-alfils(i)(j) :: List())
-    }
-  }
-
-  //e.addEOLog(alfils(0)(0) :: alfils(n-1)(n-1) :: List())
-  //e.addEOLog(alfils(0)(n-1) :: alfils(n-1)(0) :: List())
-
-  //A cada contradiagonal hi ha com a molt un alfil
-  for (v <- 0 to 2 * n - 2) {
-    val l = (for (i <- 0 until n; j <- 0 until n; if i + j == v) yield alfils(i)(j)).toList
-    if(configuracioLog) e.addAMOLog(l)
-    else e.addAMOQuad(l)
-  }
-
-  //A cada diagonal hi ha com a molt un alfil
-  for (v <- -n + 1 until n) {
-    val l = (for (i <- 0 until n; j <- 0 until n; if i - j == v) yield alfils(i)(j)).toList
-    if(configuracioLog) e.addAMOLog(l)
-    else e.addAMOQuad(l)
-  }
-
-  if(nAlfils >= 2*(n-1)) {
-    //Reifiquem (????????????????????????) caselles amb alfil i diagonals
-    for (v <- -n + 1 until n) {
-      var casellesDiag = (for (i <- 0 until n; j <- 0 until n; if i - j == v) yield alfils(i)(j)).toList;
-      for (i <- casellesDiag.indices) e.addClause(-casellesDiag(i) :: diagonals(v + n - 1) :: List())
-      //e.addClause(List(-diagonals(v + n - 1)).concat(casellesDiag))
-      e.addALO(List(-diagonals(v + n - 1)).concat(casellesDiag))
-    }
-
-    //Hi ha d'haver exactament 2*(n-1) diagonals amb un alfil
-    e.addEK(diagonals.toList, 2 * (n - 1))
-
-    //Reifiquem (????????????????????????) caselles amb alfil i contradiagonals
-    //Amb això va més ràpid per alguna raó
-    /*for (v <- -n + 1 until n) {
-      var casellesContradiag = (for (i <- 0 until n; j <- 0 until n; if i - j == v) yield alfils(i)(j)).toList;
-      for (i <- casellesContradiag.indices) e.addClause(-casellesContradiag(i) :: diagonals(v + n - 1) :: List())
-      //e.addClause(List(-contradiagonals(v + n - 1)).concat(casellesContradiag))
-      e.addALO(List(-contradiagonals(v + n - 1)).concat(casellesContradiag))
-    }*/
-
-    for (v <- 0 to 2 * n - 2) {
-      var casellesContradiag = (for (i <- 0 until n; j <- 0 until n; if i + j == v) yield alfils(i)(j)).toList;
-      for (i <- casellesContradiag.indices) e.addClause(-casellesContradiag(i) :: contradiagonals(v) :: List())
-      //e.addClause(List(-contradiagonals(v + n - 1)).concat(casellesContradiag))
-      e.addALO(List(-contradiagonals(v)).concat(casellesContradiag))
-    }
-
-    //Hi ha d'haver exactament 2*(n-1) diagonals amb un alfil
-    e.addEK(contradiagonals.toList, 2 * (n - 1))
-  }
-
-
-
-  // CAVALLS
-
-  //val possibleMoves = List((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
-  val possibleMoves = List((1, -2), (-1, -2), (2, -1), (-2, -1))
-  for (x <- 0 until n; y <- 0 until n)
-    for (i <- possibleMoves; if i._1+x >= 0 && i._2+y >= 0 && i._1+x < n && i._2+y < n)
-      if(configuracioLog) e.addAMOLog(List(cavalls(x)(y),cavalls(i._1+x)(i._2+y)))
-      else e.addAMOQuad(List(cavalls(x)(y),cavalls(i._1+x)(i._2+y)))
-      //e.addClause(-cavalls(x)(y) :: -cavalls(i._1+x)(i._2+y) :: List())*/
-
+  //val possibleMovesReis = List((1, 0), (1, -1), (0, -1), (-1, -1), (-1,0), (-1,1), (0,1), (1,1))
   val possibleMovesReis = List((1, 0), (1, -1), (0, -1), (-1, -1))
   def afegirConstraintsReis(e: ScalAT, nReis: Int): Unit = {
     afegirConstraints(e)
     reis = e.newVar2DArray(n, n)
-    println("AFEGIM CONSTRAINT de ", nReis, " reis")
+    println("Utilitzem el constraint d'exactament " ++ nReis.toString ++ " reis.")
     e.addEK(reis.flatten.toList, nReis)
     for (i <- 0 until n)
       for (j <- 0 until n) {
@@ -467,7 +285,7 @@ object CrowdedChessboard extends App{
         } else if (cavallsAux(i)(j)) {
           print("C ")
         } else if (placeKings && reisAux(i)(j)) {
-          print ("K ")
+          print (s"${GREEN}K ${RESET}")
         } else{
           print(". ")
         }
@@ -476,9 +294,6 @@ object CrowdedChessboard extends App{
     }
   }
 
-  val result = e.solve()
-  println(result)
-
   def actualitzarAuxiliars(e: ScalAT, actualitzarReis: Boolean): Unit = {
     for (i <- reines.indices) {
       for (j <- reines.indices) {
@@ -486,19 +301,30 @@ object CrowdedChessboard extends App{
         torresAux(i)(j) = e.getValue(torres(i)(j))
         alfilsAux(i)(j) = e.getValue(alfils(i)(j))
         cavallsAux(i)(j) = e.getValue(cavalls(i)(j))
-        if(actualitzarReis) reisAux(i)(j) = e.getValue(reis(i)(j))
+        if (actualitzarReis) reisAux(i)(j) = e.getValue(reis(i)(j))
       }
     }
   }
 
-  //Reis
+  /*************************
+   * * * * * * * * * * * * *
+   * Obtenció de solucions *
+   * * * * * * * * * * * * *
+   *************************/
+
+  var e = new ScalAT("CrowdedChessboard")
+  var e2: ScalAT = null
+  //Afegim tots els constraints a e
+  afegirConstraints(e)
+  val result = e.solve()
+  println(result)
+
+  //Si hem de trobar el nombre màxim de reis
   if(result.satisfiable && placeKings){
     actualitzarAuxiliars(e, false)
     val emptySpaces = n*n - nReines - nAlfils - nTorres - nCavalls
-    //val possibleMovesReis = List((1, 0), (1, -1), (0, -1), (-1, -1), (-1,0), (-1,1), (0,1), (1,1))
     e2 = new ScalAT("CrowdedChessboardKings")
     reis = e2.newVar2DArray(n,n)
-    //val possibleMovesReis = List((1, 0), (1, -1), (0, -1), (-1, -1))
     //Llista de caselles ocupades per altres peces
     val l = (for(i <- 0 until n; j <- 0 until n; if(
       e.getValue(reines(i)(j)) |  e.getValue(torres(i)(j)) |  e.getValue(alfils(i)(j)) |  e.getValue(cavalls(i)(j))
@@ -524,7 +350,7 @@ object CrowdedChessboard extends App{
       result2 = e2.solve()
       kingsTime += result2.time
       if (result2.satisfiable) {
-        println("ES SATISFACIBLE AMB " ++ nReis.toString())
+        println("* Determinat que com a mínim és satisfactible amb " ++ nReis.toString() ++ " rei(s) *")
         for (i <- reines.indices) {
           for (j <- reines.indices) {
             reisAux(i)(j) = e2.getValue(reis(i)(j))
@@ -533,16 +359,19 @@ object CrowdedChessboard extends App{
       }
     } while (result2.satisfiable & nReis < emptySpaces)
 
-    println("***********************")
-    if(!result2.satisfiable) println("A la solució inicial hi podem posar " ++ (nReis - 1).toString ++ " Reis")
-    else println("En total podem posar " ++ nReis.toString ++ " Reis")
+    println("**********************************************************")
+    if(!result2.satisfiable) {
+      println("A la solució inicial hi podem posar " ++ (nReis - 1).toString ++ " rei(s).")
+      println("Ara buscarem si hi ha alguna distribució que accepti més reis.")
+    }
+    else println("En total podem posar " ++ nReis.toString ++ " reis, ja que hem omplert tots els espais buits")
     println("King's Time: " ++ kingsTime.toString ++ "\n")
 
     var maximNombreTrobat = false
     while(nReis < emptySpaces && !maximNombreTrobat) {
       e = new ScalAT("CrowdedChessboard")
 
-      println("Provem amb " ++ nReis.toString ++ " Reis")
+      println("Provem-ho amb " ++ nReis.toString ++ " reis:")
       afegirConstraintsReis(e, nReis)
 
       result2 = e.solve()
@@ -550,21 +379,22 @@ object CrowdedChessboard extends App{
       println("King's time: " ++ kingsTime.toString)
 
       if (result2.satisfiable) {
-        println("ES SATISFACIBLE AIXI QUE ACTUALITZEM REIS!")
+        println("Satisfactible. Provem-ho amb un rei més.\n")
         actualitzarAuxiliars(e, true)
         nReis += 1
       }
       else {
         maximNombreTrobat = true
-        println("El màxim nombre de reis que podem posar és " ++ (nReis - 1).toString)
+        println("No és satisfactible.\n")
+        println("Per tant, el màxim nombre de reis que podem posar són " ++ (nReis - 1).toString ++ " rei(s):\n")
       }
     }
 
-    println(getTauler)
+    getTauler
   }
-
+    // Si no hem de trobar el nombre màxim de reis
   else if (!placeKings && result.satisfiable) {
     actualitzarAuxiliars(e, false)
-    println(getTauler)
+    getTauler
   }
 }
